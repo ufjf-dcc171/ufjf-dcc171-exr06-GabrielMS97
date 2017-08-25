65004c67c605aaf975cb39fa73d2c48312d3a8fe
Gabriel <Gabriel@Gabriel-PC>
2017-08-25 10:50:33 -0300

    Exercício concluído

--- src/br/ufjf/dcc171/Aula06Exr2.java
+++ src/br/ufjf/dcc171/Aula06Exr2.java
@@ -6,7 +6,7 @@ public class Aula06Exr2 {
 
     public static void main(String[] args) {
         Janela j = new Janela();
-        j.setSize(250, 200);
+        j.setSize(240, 200);
         j.setVisible(true);
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         j.setLocationRelativeTo(null);
--- src/br/ufjf/dcc171/Janela.java
+++ src/br/ufjf/dcc171/Janela.java
@@ -14,17 +14,22 @@ import javax.swing.JRadioButton;
 
 class Janela extends JFrame {
 
-    private final JLabel etiqueta = new JLabel("Escolha sua opção");
-    private final JLabel computador = new JLabel("Jogada da máquina: ");
-    private final JRadioButton pedra = new JRadioButton("Pedra", true);
-    private final JRadioButton papel = new JRadioButton("Papel", false);
-    private final JRadioButton tesoura = new JRadioButton("Tesoura", false);
-    private final JButton jogar = new JButton("Jogar");
-
     private int vitorias = 0;
     private int derrotas = 0;
     private int empates = 0;
     private int rodadas = 0;
+    
+    private final JLabel etiqueta = new JLabel("Escolha sua opção");
+    private final JLabel computador = new JLabel("Jogada da máquina: ");
+    private final JLabel mensagem = new JLabel("");
+    private final JLabel rodada = new JLabel("Rodada atual: " + rodadas);
+    private final JLabel vitoria = new JLabel("Vitórias " + vitorias);
+    private final JLabel empate = new JLabel("Empates: " + empates);
+    private final JLabel derrota = new JLabel("Derrotas " + derrotas);
+    private final JRadioButton pedra = new JRadioButton("Pedra", true);
+    private final JRadioButton papel = new JRadioButton("Papel", false);
+    private final JRadioButton tesoura = new JRadioButton("Tesoura", false);
+    private final JButton jogar = new JButton("Jogar");
 
     public Janela() throws HeadlessException {
         super("Pedra-Papel-Tesoura");
@@ -36,6 +41,11 @@ class Janela extends JFrame {
         add(tesoura);
         add(jogar);
         add(computador);
+        add(rodada);
+        add(vitoria);
+        add(empate);
+        add(derrota);
+        add(mensagem);
 
         ButtonGroup opcoes = new ButtonGroup();
 
@@ -74,6 +84,52 @@ class Janela extends JFrame {
             }
 
             //JOptionPane.showMessageDialog(null, "Saída: " + jogada, "A", JOptionPane.INFORMATION_MESSAGE);
+            if (jogada == saida) {
+                empates++;
+                mensagem.setText("Empate!!");
+            } else if (saida == jogada - 1) {
+                vitorias++;
+                mensagem.setText("Vitória!!");
+            } else if (saida == jogada + 1) {
+                derrotas++;
+                mensagem.setText("Derrota :(");
+            } else if (jogada == 0 && saida == 2) {
+                vitorias++;
+                mensagem.setText("Vitória!!");
+            } else if (jogada == 2 && saida == 0) {
+                derrotas++;
+                mensagem.setText("Derrota :(");
+            }
+            
+            //System.out.println("vitórias: " + vitorias);
+            //System.out.println("derrotas: " + derrotas);
+            //System.out.println("empates: " + empates);
+            //System.out.println("rodadas: "+ rodadas);
+            
+            rodada.setText("Rodada atual: " + rodadas);
+            vitoria.setText("Vitórias " + vitorias);
+            derrota.setText("Derrotas " + derrotas);
+            empate.setText("Empates: " + empates);
+            
+            if(rodadas == 7)
+            {
+                if(vitorias > derrotas)
+                {
+                    JOptionPane.showMessageDialog(null, "Você venceu a rodada!!", "Vitória!!!!", JOptionPane.INFORMATION_MESSAGE);
+                }
+                else if(derrotas > vitorias)
+                {
+                    JOptionPane.showMessageDialog(null, "Você perdeu a rodada", "Derrota!", JOptionPane.INFORMATION_MESSAGE);
+                }
+                else if(derrotas == vitorias)
+                {
+                    JOptionPane.showMessageDialog(null, "A rodada terminou empatada", "Empate!", JOptionPane.INFORMATION_MESSAGE);
+                }
+                rodadas = 0;
+                vitorias = 0;
+                derrotas = 0;
+                empates = 0;
+            }
         }
 
     }
